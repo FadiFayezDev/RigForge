b306abadf1985602ac957edf0e9a5a191f0e98f3
FadiFayezDev <fadyfayez.net@gmail.com>
2026-09-05 07:35:26 +0300

    RIG-36 Define CPU Repository Contracts

---
+++ src/Modules/CPUModule/CPUModule.Application/Repositories/ISocketProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Application.Common.Interfaces;
+using CPUModule.Domain.Primitives.Identifiers;
+using Domain.Entities.Socket;
+
+namespace CPUModule.Application.Repositories
+{
+    public interface ISocketProfileRepository : IRepository<SocketProfile, SocketProfileId>
+    {
+    }
+}
